b004e8e32a2ea966ce4949d0b734138cfdd1e874
Daniel Larrea <larread6286@my.uwstout.edu>
2018-09-27 08:44:21 -0500

    Added Basic Water Sprays
    Added 2 water spray objects, one down and one forward.

---
+++ The Interloper/Assets/Scripts/ToggleWaterSpray.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleWaterSpray : MonoBehaviour {
+
+	private ParticleSystem WaterSpray;
+
+	// Use this for initialization
+	void Start () {
+		WaterSpray = GetComponent<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.P)) {
+			WaterSpray.Play ();
+		} else {
+			WaterSpray.Stop ();
+		}
+	}
+}
