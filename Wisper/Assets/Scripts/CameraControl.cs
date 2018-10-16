6db6e8ff41aec6475d45e841c980e32a94dc899d
Daniel Larrea <larread6286@my.uwstout.edu>
2018-10-16 10:54:42 -0500

    Revert "Merge branch 'master' of https://github.com/Snaps56/Wisper"
    This reverts commit 8cca542913b62317029d5eb9917ae7179aed8493, reversing
    changes made to 3105d47748a78bd48d1d8da9c89b35e0f89f1981.

---
+++ Wisper/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 44.0f;
+
+    public Transform lookAt;
+    public Transform camTransform;
+    public float distance = 10.0f;
+
+    private float currentX = 0.0f;
+    private float currentY = 22.0f;
+    public float sensitivityX = 4.0f;
+    public float sensitivityY = 1.0f;
+
+    private void Start()
+    {
+        camTransform = transform;
+    }
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("HorizontalR");
+        currentY += Input.GetAxis("VerticalR");
+
+        // mouse look
+        currentX += Input.GetAxis("Mouse X");
+        currentY += Input.GetAxis("Mouse Y");
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY*sensitivityY, currentX*sensitivityX, 0);
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
+    }
+}
