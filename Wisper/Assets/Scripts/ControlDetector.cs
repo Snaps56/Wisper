fcadb772e507f0b1025e8f78d16c02ca8ad1ca54
Cheenue Yang <31903854+YangCheenue@users.noreply.github.com>
2018-10-18 18:45:49 -0500

    Control Tutorials Toggle
    Control Tutorials are toggled based on the user's control scheme.

---
+++ Wisper/Assets/Scripts/ControlDetector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlDetector : MonoBehaviour {
+
+    public bool isUsingController;
+    public GameObject controllerObject;
+    public GameObject keyboardMouseObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isUsingController)
+        {
+            controllerObject.SetActive(true);
+            keyboardMouseObject.SetActive(false);
+        }
+        else
+        {
+            controllerObject.SetActive(false);
+            keyboardMouseObject.SetActive(true);
+        }
+	}
+}
