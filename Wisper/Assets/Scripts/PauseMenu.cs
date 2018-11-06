2a32fa9c63d9ace3388d70a1f9d1868b3be21661
Chris Schrempp <schremppc0870@my.uwstout.edu>
2018-11-06 10:54:17 -0600

    Added Audio Features to Pause Menu

--- Wisper/Assets/Scripts/PauseMenu.cs
+++ Wisper/Assets/Scripts/PauseMenu.cs
@@ -12,6 +12,7 @@ public class PauseMenu : MonoBehaviour
     public GameObject settingsMenu;
     public GameObject VideoMenu;
     public AudioMixer audioMixer;
+    public float CurrentVolume;
 
     // Update is called once per frame
     void Update()
@@ -38,6 +39,7 @@ public class PauseMenu : MonoBehaviour
         GameIsPaused = false;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        SetVolume(CurrentVolume + 15);
     }
     // Pauses the game and unlocks the cursor
     void Pause()
@@ -47,6 +49,7 @@ public class PauseMenu : MonoBehaviour
         GameIsPaused = true;
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
+        SetVolume(CurrentVolume - 15);
     }
     // Quits the game! But why would you want to do that?
     public void QuitGame()
@@ -57,6 +60,7 @@ public class PauseMenu : MonoBehaviour
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("volume", volume);
+        CurrentVolume = volume;
     }
 }
 
