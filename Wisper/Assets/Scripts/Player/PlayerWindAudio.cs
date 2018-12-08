df165a52a78dd1a74410c8cb318aa36e02cc0cac
Cheenue Yang <31903854+YangCheenue@users.noreply.github.com>
2018-12-08 16:28:55 -0600

    Moved a few Scripts in Assets

---
+++ Wisper/Assets/Scripts/Player/PlayerWindAudio.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PlayerWindAudio : MonoBehaviour {
+
+    public Rigidbody playerrigidbody;
+    public AudioSource source;
+    private float finalVelocity;
+
+	// Use this for initialization
+	void Start () {
+        source.volume = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerrigidbody.velocity.magnitude > 1)
+        {
+            if (1 - 1 / playerrigidbody.velocity.magnitude > 0.5)
+            {
+                source.volume = (1 - 1 / playerrigidbody.velocity.magnitude) * 0.5f;
+                source.pitch = (1 - 1 / playerrigidbody.velocity.magnitude) * 2;
+            }
+        }
+        else
+        {
+            source.volume = 0.25f;
+            source.pitch = 1f;
+        }
+	}
+}
