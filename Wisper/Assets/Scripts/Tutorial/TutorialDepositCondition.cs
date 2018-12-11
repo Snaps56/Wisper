09f980a4e9e0778ba6891da75328a78f7d19ec06
Cheenue Yang <31903854+YangCheenue@users.noreply.github.com>
2018-12-11 01:50:13 -0600

    Added Deposit Orbs Condition

---
+++ Wisper/Assets/Scripts/Tutorial/TutorialDepositCondition.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialDepositCondition : MonoBehaviour {
+
+    public string [] dependentConditions;
+
+    private TutorialCondition tutorialCondition;
+    private PersistantStateData persistantStateData;
+    private bool currentConditionsMet;
+    private bool conditionCheck;
+
+    private bool conditionDone;
+
+    // Use this for initialization
+    void Start () {
+        tutorialCondition = GetComponent<TutorialCondition>();
+        persistantStateData = GameObject.Find("PersistantStateData").GetComponent<PersistantStateData>();
+        conditionDone = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (conditionCheck && !conditionDone)
+        {
+            tutorialCondition.SetCondition(true);
+            conditionDone = true;
+        }
+        else
+        {
+            currentConditionsMet = true;
+            for (int i = 0; i < dependentConditions.Length; i++)
+            {
+                bool currentCondition = (bool)persistantStateData.stateConditions[dependentConditions[i]];
+                if (!currentCondition)
+                {
+                    currentConditionsMet = false;
+                }
+            }
+            if (currentConditionsMet)
+            {
+                conditionCheck = true;
+            }
+        }
+    }
+}
