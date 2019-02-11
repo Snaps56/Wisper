33fef1fd52aaecf8ef57bad26fef1cd2db67c82c
kuekie0 <thaok0884@my.uwstout.edu>
2019-02-10 20:30:29 -0600

    BirdTask

---
+++ Wisper/Assets/Scripts/Task & Orb/BirdTask.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdTask : MonoBehaviour {
+
+    
+    public GameObject finalPlace;
+    private Vector3 returned;
+
+    private bool getOrbs;
+    private bool pickedUp;
+    private bool letGo;
+    private float height;
+    // Use this for initialization
+    void Start()
+    {
+        pickedUp = false;
+        getOrbs = true;
+        letGo = false;
+        height = 0.2f;
+    }
+
+    private void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == finalPlace.tag)
+        {
+            pickedUp = true;
+        }
+    }
+
+    void PickUp()
+    {
+        returned = new Vector3(finalPlace.transform.position.x, finalPlace.transform.position.y + height, finalPlace.transform.position.z);
+        transform.position = returned;
+        
+        if (transform.position == returned && getOrbs == true)
+        {
+            GetComponent<SpawnOrbs>().DropOrbs();
+            getOrbs = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pickedUp == true)
+        {
+            PickUp();           
+        }
+              
+    }
+}
---
+++ Wisper/Assets/Scripts/Task & Orb/KiteTask.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KiteTask : MonoBehaviour {
+
+    private float maxHeight;
+    private bool getOrbs;
+    private Vector3 finalPosition;
+    private float finalX;
+    private float finalY;
+    private float finalZ;
+
+	// Use this for initialization
+	void Start () {
+        maxHeight = 50.0f;
+        getOrbs = true;
+        finalX = 63.0f;
+        finalY = 51.0f;
+        finalZ = -2.0f;
+        finalPosition = new Vector3(finalX, finalY, finalZ);
+    }
+	
+    void FlyKite()
+    {
+        
+        if (transform.position.y >= maxHeight && getOrbs == true)
+        {
+            transform.position = finalPosition;
+            GetComponent<SpawnOrbs>().DropOrbs();
+            getOrbs = false;
+        }
+        
+    }
+
+	// Update is called once per frame
+	void Update () {
+        FlyKite();
+	}
+}
