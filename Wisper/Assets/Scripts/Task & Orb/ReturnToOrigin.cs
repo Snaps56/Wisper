31ff2b57d6bea20e74eeba05b0a283a6237fdf6d
Nick <nmnorton56@gmail.com>
2019-02-25 14:19:43 -0600

    Started Task Script

---
+++ Wisper/Assets/Scripts/Task & Orb/ReturnToOrigin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToOrigin : MonoBehaviour {
+
+    private Vector3 startPos;
+
+    // Use this for initialization
+    void Start () {
+        startPos = this.transform.position;
+	}
+	
+    //Return back to it's original position
+    void OnTriggerEnter (Collider other) {
+        if (other.CompareTag("Border")) {
+            this.transform.position = startPos;
+            this.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
+        }
+    }
+
+}
---
+++ Wisper/Assets/Scripts/Task & Orb/TaskHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskManager : MonoBehaviour {
+
+    private SpawnOrbs spawnOrbs;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.L))
+        {
+            spawnOrbs.DropOrbs();
+        }
+
+    }
+}
