ac86e52771041ac3f6f6f40e6acee5dee133309c
Cheenue Yang <31903854+YangCheenue@users.noreply.github.com>
2019-02-26 19:25:58 -0600

    Added MouseModule script to fix menu controls

---
+++ Wisper/Assets/Scripts/UI/MouseModule.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MouseModule : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private EventSystem masterEventSystem;
+    private bool mouseOverObject = false;
+    private GameObject currentGameObject;
+
+    void Start()
+    {
+        masterEventSystem = EventSystem.current;
+        currentGameObject = masterEventSystem.currentSelectedGameObject;
+    }
+    void Update()
+    {
+        currentGameObject = masterEventSystem.currentSelectedGameObject;
+        if (currentGameObject != gameObject && mouseOverObject)
+        {
+            masterEventSystem.SetSelectedGameObject(gameObject);
+        }
+        Debug.Log(currentGameObject);
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouseOverObject = true;
+        masterEventSystem.SetSelectedGameObject(gameObject);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouseOverObject = false;
+    }
+
+}
