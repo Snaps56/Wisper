81a3c66841cce690aa6fed8c2c279ebe9de68228
Daniel Larrea <larread6286@my.uwstout.edu>
2019-03-01 15:27:05 -0600

    Created a Sling thing for testing pushing and pulling

---
+++ Wisper/Assets/Scripts/RedrawLine.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedrawLine : MonoBehaviour {
+
+	public LineRenderer[] lines;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (LineRenderer line in lines) {
+			line.SetPosition (1, transform.localPosition);
+		}
+	}
+}
