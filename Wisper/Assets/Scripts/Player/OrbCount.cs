d528f569b1110b5921a35d7293d71d41f0ff4575
Nicholas Norton <nmnorton56@gmail.com>
2019-03-04 12:00:48 -0600

    Fixed the UI

--- Wisper/Assets/Scripts/Player/OrbCount.cs
+++ Wisper/Assets/Scripts/Player/OrbCount.cs
@@ -24,14 +24,16 @@ public class OrbCount : MonoBehaviour {
     {
         psd = PersistantStateData.persistantStateData;
 
-        TotalOrbCount = (float)psd.stateConditions["TotalTasks"] * 5;
-        psd.ChangeStateConditions("TotalOrbCount", TotalOrbCount);
+        //TotalOrbCount = (float)psd.stateConditions["TotalTasks"] * 5;
+        //psd.ChangeStateConditions("TotalOrbCount", TotalOrbCount);
 
-        orbMaxDeposit = TotalOrbCount / 2;
-        psd.ChangeStateConditions("OrbMaxDeposit", orbMaxDeposit);
+        //orbMaxDeposit = TotalOrbCount / 2;
+        //psd.ChangeStateConditions("OrbMaxDeposit", orbMaxDeposit);
+
+        orbMaxDeposit = (float)psd.stateConditions["OrbMaxDeposit"];
 
         orbCount = (float)psd.stateConditions["OrbCount"];
-        orbCountText.text = (float)psd.stateConditions["OrbCount"] + "/" + Mathf.Ceil(orbMaxDeposit);
+        orbCountText.text = (float)psd.stateConditions["OrbCount"] + "/" + orbMaxDeposit;
     }
 
     //Sets the orb count
