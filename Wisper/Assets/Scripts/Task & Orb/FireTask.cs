5b04a4e46db5b746e4f9e76b3d7511db8ee80355
Chris Schrempp <schremppc0870@my.uwstout.edu>
2019-03-03 21:31:56 -0600

    Added Fire Particle to Fire Task

--- Wisper/Assets/Scripts/Task & Orb/FireTask.cs
+++ Wisper/Assets/Scripts/Task & Orb/FireTask.cs
@@ -13,7 +13,15 @@ public class FireTask : MonoBehaviour {
     private bool isThrowing;
     public Camera mainCamera;
     public float dotProductAngle = 0.9f;
-    public float deactivateDistance = 10;
+    public float deactivateDistance = 5;
+
+    //particle variables
+    public ParticleSystem Particle_Fire;
+    public ParticleSystem Particle_Ember_Flame;
+    private ParticleSystem.MainModule mainModule;
+    private float ParticleSize = 0;
+    private float ParticleSpeed = 0;
+    private int count;
 
     // Vibration Variables
     bool playerIndexSet = false;
@@ -35,6 +43,7 @@ public class FireTask : MonoBehaviour {
     void Start()
     {
         persistantStateData = GameObject.Find("PersistantStateData").GetComponent<PersistantStateData>();
+        mainModule = Particle_Ember_Flame.main;
     }
 
     IEnumerator Wait()
@@ -43,8 +52,8 @@ public class FireTask : MonoBehaviour {
         GamePad.SetVibration(playerIndex, 0f, 0f);
     }
 
-    // Update is called once per frame
-    void Update () {
+    void FixedUpdate()
+    {
         //Update the forward vector
         cameraForward = mainCamera.transform.forward;
 
@@ -53,23 +62,90 @@ public class FireTask : MonoBehaviour {
 
         float distance = Vector3.Distance(player.transform.position, transform.position);
 
-        if ((bool)persistantStateData.stateConditions["ShrineFirstConversationOver"])
+        if (Vector3.Dot(cameraForward.normalized, toTask.normalized) > (dotProductAngle) && distance <= deactivateDistance)
         {
-            //Player is looking at shrine
-            if (Vector3.Dot(cameraForward.normalized, toTask.normalized) > (dotProductAngle) && distance <= deactivateDistance)
+            if (TaskisDone == false && ability.GetComponent<ObjectThrow>().GetIsThrowingObjects() == true)
             {
-                if (TaskisDone == false && ability.GetComponent<ObjectThrow>().GetIsThrowingObjects() == true)
-                {
-                    // Task is complete
-                    TaskisDone = true;
-                    GetComponent<SpawnOrbs>().DropOrbs();
-                    persistantStateData.stateConditions["FireTaskDone"] = true;
-                    persistantStateData.updateCount++;
-                    GamePad.SetVibration(playerIndex, 0f, 1f);
-                    StartCoroutine(Wait());
-                }
+                count++;
+                ParticleSize += 0.006f;
+                ParticleSpeed += 0.01f;
+                mainModule.startSize = ParticleSize;
+                mainModule.startSpeed = ParticleSpeed;
+                
+                Debug.Log(ParticleSize);
 
+                if (!Particle_Ember_Flame.isPlaying)
+                {
+                    Particle_Ember_Flame.Play();
+                    
+                }
+            }
+
+            if (TaskisDone == false && ability.GetComponent<ObjectThrow>().GetIsThrowingObjects() == false && count != 0)
+            {
+                count--;
+                ParticleSize -= 0.006f;
+                ParticleSpeed -= 0.01f;
+                mainModule.startSize = ParticleSize;
+                mainModule.startSpeed = ParticleSpeed;
+
+                if (Particle_Ember_Flame.isPlaying)
+                {
+                    Particle_Ember_Flame.Stop();
+                }
             }
         }
+
+        //  if ((bool)persistantStateData.stateConditions["ShrineFirstConversationOver"])
+        // {
+        //Player is looking at shrine
+        if (Vector3.Dot(cameraForward.normalized, toTask.normalized) > (dotProductAngle) && distance <= deactivateDistance)
+        {
+            if (TaskisDone == false && count >= 200)
+            {
+                // Task is complete
+                TaskisDone = true;
+                Particle_Fire.Play();
+                Particle_Ember_Flame.Stop();
+                GetComponent<SpawnOrbs>().DropOrbs();
+                persistantStateData.stateConditions["FireTaskDone"] = true;
+                persistantStateData.updateCount++;
+                GamePad.SetVibration(playerIndex, 0f, 1f);
+                StartCoroutine(Wait());
+            }
+
+        }
+        //}
+    }
+
+    // Update is called once per frame
+    void Update () {
+        ////Update the forward vector
+        //cameraForward = mainCamera.transform.forward;
+
+        ////Find direction between the shrine and camera
+        //toTask = firepit.transform.position - mainCamera.transform.position;
+
+        //float distance = Vector3.Distance(player.transform.position, transform.position);
+
+      ////  if ((bool)persistantStateData.stateConditions["ShrineFirstConversationOver"])
+      // // {
+      //      //Player is looking at shrine
+      //      if (Vector3.Dot(cameraForward.normalized, toTask.normalized) > (dotProductAngle) && distance <= deactivateDistance)
+      //      {
+      //          if (TaskisDone == false && ability.GetComponent<ObjectThrow>().GetIsThrowingObjects() == true)
+      //          {
+      //              // Task is complete
+      //              TaskisDone = true;
+      //              fire.Play();
+      //              GetComponent<SpawnOrbs>().DropOrbs();
+      //              persistantStateData.stateConditions["FireTaskDone"] = true;
+      //              persistantStateData.updateCount++;
+      //              GamePad.SetVibration(playerIndex, 0f, 1f);
+      //              StartCoroutine(Wait());
+      //          }
+
+      //      }
+      //  //}
     }
 }
