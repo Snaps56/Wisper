542acf170dad7ca21f29b725df8717716dc9e04d
Cheenue Yang <31903854+YangCheenue@users.noreply.github.com>
2019-03-10 00:50:13 -0600

    Sand Castle Task Mostly Complete

---
+++ Wisper/Assets/Scripts/SandParticlesTask.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandParticlesTask : MonoBehaviour {
+
+    private Rigidbody rb;
+    private ParticleSystem sandParticleSystem;
+    private ParticleSystem.EmissionModule emissionModule;
+    private ParticleSystem.MainModule mainModule;
+    private float initialEmission;
+    private float initialLifetime;
+    private float newLifetime = 99999999999;
+
+    private bool isThrowing;
+    private bool isLifting;
+
+    private bool modifiedStationaryParticles = false;
+    private ParticleSystem.Particle[] currentParticles;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        sandParticleSystem = GetComponent<ParticleSystem>();
+        emissionModule = sandParticleSystem.emission;
+        mainModule = sandParticleSystem.main;
+        initialEmission = emissionModule.rateOverTime.constant;
+        initialLifetime = mainModule.startLifetime.constant;
+
+        isThrowing = GameObject.Find("Player").GetComponent<ObjectThrow>().GetIsThrowingObjects();
+        isLifting = GameObject.Find("Player").GetComponent<ObjectLift>().GetIsLiftingObjects();
+        currentParticles = new ParticleSystem.Particle[sandParticleSystem.main.maxParticles];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(rb.velocity.magnitude);
+		if (rb.velocity.magnitude > 0.1f)
+        {
+            emissionModule.rateOverTime = initialEmission;
+            mainModule.startLifetime = initialLifetime;
+            modifiedStationaryParticles = false;
+        }
+        else
+        {
+            if (!modifiedStationaryParticles)
+            {
+                currentParticles = new ParticleSystem.Particle[sandParticleSystem.GetParticles(currentParticles)];
+                for (int i = 0; i < currentParticles.Length; i++)
+                {
+                    currentParticles[i].remainingLifetime = newLifetime;
+                }
+                sandParticleSystem.SetParticles(currentParticles, currentParticles.Length - 1);
+                emissionModule.rateOverTime = 0;
+                modifiedStationaryParticles = true;
+            }
+        }
+	}
+}
---
+++ Wisper/Assets/Scripts/Task & Orb/SandCastleTask.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandCastleTask : MonoBehaviour {
+
+    public Material transparentMaterial;
+    public Material finalMaterial;
+    public ParticleSystem sandCastleParticles;
+    public ParticleSystem sandCastleFinishParticles;
+    private float currentInterp = 0;
+    private MeshRenderer sandMeshRenderer;
+    private float materialInterpDuration;
+    private float materialInterpRate = 0.01f;
+    private float finishSandThreshold = 0.95f;
+
+    private SpawnOrbs spawnOrbsScript;
+    private bool finishedTask = false;
+
+    private Color sandColor;
+
+	// Use this for initialization
+	void Start () {
+        sandMeshRenderer = GetComponent<MeshRenderer>();
+        sandColor = transparentMaterial.color;
+        sandColor.a = 0.1f;
+        sandMeshRenderer.material = transparentMaterial;
+
+        finishedTask = (bool)PersistantStateData.persistantStateData.stateConditions["FinishedSandCastleTask"];
+
+        if (finishedTask)
+        {
+            FinishSandCastleTask();
+        }
+
+        Debug.Log(materialInterpRate);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!finishedTask)
+        {
+            sandMeshRenderer.material.color = sandColor;
+        }
+	}
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name == "SandParticle" && !finishedTask)
+        {
+            if (sandColor.a < finishSandThreshold)
+            {
+                sandColor.a += materialInterpRate;
+                if (sandCastleParticles.isStopped)
+                {
+                    sandCastleParticles.Play();
+                }
+                if (sandColor.a < 1 && sandColor.a > finishSandThreshold)
+                {
+                    if (sandCastleFinishParticles.isStopped)
+                    {
+                        sandCastleFinishParticles.Play();
+                    }
+                }
+            }
+            else
+            {
+                if (sandCastleParticles.isPlaying)
+                {
+                    FinishSandCastleTask(other);
+                }
+            }
+        }
+    }
+    void FinishSandCastleTask(Collider other)
+    {
+        sandCastleFinishParticles.Stop();
+        sandMeshRenderer.material = finalMaterial;
+        other.gameObject.GetComponent<ParticleSystem>().Stop();
+        other.gameObject.GetComponent<Collider>().enabled = false;
+        sandCastleParticles.Stop();
+
+        spawnOrbsScript.DropOrbs();
+        PersistantStateData.persistantStateData.stateConditions["FinishedSandCastleTask"] = true;
+    }
+    void FinishSandCastleTask()
+    {
+        sandCastleFinishParticles.Stop();
+        sandMeshRenderer.material = finalMaterial;
+        sandCastleParticles.Stop();
+    }
+}
