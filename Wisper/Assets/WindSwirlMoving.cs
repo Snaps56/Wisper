4f5c77886d0ec66eec18be02bb72592de00b49ba
Cheenue Yang <31903854+YangCheenue@users.noreply.github.com>
2019-03-10 00:49:50 -0600

    Testing Additional Wind Particles

---
+++ Wisper/Assets/WindSwirlMoving.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindSwirlMoving : MonoBehaviour {
+
+    private PlayerMovement movementScript;
+    private ParticleSystem mainParticleSystem;
+    private ParticleSystem.VelocityOverLifetimeModule psVelocityLifetime;
+
+    private AnimationCurve zAnimationCurve;
+    private ParticleSystem.MinMaxCurve zVelocityCurve;
+    private AnimationCurve yAnimationCurve;
+    private ParticleSystem.MinMaxCurve yVelocityCurve;
+
+
+    private float playerSpeed;
+
+	// Use this for initialization
+	void Start () {
+        movementScript = GameObject.Find("Player").GetComponent<PlayerMovement>();
+        mainParticleSystem = GetComponent<ParticleSystem>();
+        psVelocityLifetime = mainParticleSystem.velocityOverLifetime;
+        zAnimationCurve = psVelocityLifetime.z.curve;
+        zVelocityCurve = new ParticleSystem.MinMaxCurve(1, zAnimationCurve);
+        yAnimationCurve = psVelocityLifetime.y.curve;
+        yVelocityCurve = new ParticleSystem.MinMaxCurve(1, yAnimationCurve);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        playerSpeed = movementScript.GetVelocity().magnitude;
+        zVelocityCurve = new ParticleSystem.MinMaxCurve(playerSpeed, zAnimationCurve);
+        psVelocityLifetime.z = zVelocityCurve;
+
+        yVelocityCurve = new ParticleSystem.MinMaxCurve(playerSpeed * 0.3f, yAnimationCurve);
+        psVelocityLifetime.y = yVelocityCurve;
+    }
+}
