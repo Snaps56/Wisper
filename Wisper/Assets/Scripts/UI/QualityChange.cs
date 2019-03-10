c26dac77e2015e0eb0f88acb866c45d29c468fbb
Cheenue Yang <31903854+YangCheenue@users.noreply.github.com>
2019-03-10 01:02:50 -0600

    Allowed Quality Settings to change Post-Processing

--- Wisper/Assets/Scripts/UI/QualityChange.cs
+++ Wisper/Assets/Scripts/UI/QualityChange.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PostProcessing;
 
 public class QualityChange : MonoBehaviour {
 
+    public PostProcessingProfile ppProfile;
     string[] names;
 
     // Use this for initialization
@@ -17,14 +19,23 @@ public class QualityChange : MonoBehaviour {
 	}
     public void SetLow()
     {
+        ppProfile.depthOfField.enabled = false;
+        ppProfile.ambientOcclusion.enabled = false;
+        ppProfile.bloom.enabled = false; ;
         QualitySettings.SetQualityLevel(0);
     }
     public void SetMedium()
     {
+        ppProfile.depthOfField.enabled = true;
+        ppProfile.ambientOcclusion.enabled = false;
+        ppProfile.bloom.enabled = false;
         QualitySettings.SetQualityLevel(2);
     }
     public void SetHigh()
     {
+        ppProfile.depthOfField.enabled = true;
+        ppProfile.ambientOcclusion.enabled = true;
+        ppProfile.bloom.enabled = true;
         QualitySettings.SetQualityLevel(4);
     }
 }
