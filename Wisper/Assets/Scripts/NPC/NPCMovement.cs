5693808696c208f1a7f4cadf314d4acac7bb6da7
JaceAD <dembskij0659@my.uwstout.edu>
2019-03-18 15:50:51 -0500

    Added support for non-looping movement for npcs.
    If using non-looping, waypoint references in movement script will be deleted upon completion of path. (Does not delete the gameobjects themselves).
    
    Waypoints changed from array to list.
    For non-looping, please dynamically add waypoint references as necessary with another script. (EX: ShelindaPathfinder should be responsible for determining which waypoints to add to Shelinda's NPCMovement.waypoints list.).

--- Wisper/Assets/Scripts/NPC/NPCMovement.cs
+++ Wisper/Assets/Scripts/NPC/NPCMovement.cs
@@ -6,13 +6,14 @@ using UnityEngine.AI;
 public class NPCMovement : MonoBehaviour
 {
     public GameObject player;
-    public GameObject[] waypoints;
+    public List<GameObject> waypoints;
     int currentWP = 0;
     public float rotSpeed = 3.0f;
     public float speed = 0.5f;
     public float accuracyWP = 1.0f;
-
+    
     public bool move = true;
+    public bool loop = true;    // Loop movement around waypoints or stop at final waypoint (clears waypoints at end if false)
 
     // Use this for initialization
     void Start()
@@ -28,26 +29,35 @@ public class NPCMovement : MonoBehaviour
         Vector3 direction = player.transform.position - this.transform.position;
         
         //Movement loop
-        if (waypoints.Length > 0)
+        if (waypoints.Count > 0)
         {
+            //If move, transform npc toward waypoint (rot and pos with interpolation)
+            if (move == true)
+            {
+                direction = waypoints[currentWP].transform.position - transform.position;
+                this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
+                this.transform.Translate(0, 0, Time.deltaTime * speed);
+            }
+
             if (Vector3.Distance(waypoints[currentWP].transform.position, transform.position) < accuracyWP)
             {
                 currentWP++;
-				if (currentWP >= waypoints.Length) {
-					currentWP = 0;
+				if (currentWP >= waypoints.Count) {
+                    // If loop, next waypoint is 0. Else get rid of waypoints
+                    if (loop)
+                    {
+                        currentWP = 0;
+                    }
+                    else
+                    {
+                        waypoints.Clear();
+                    }
+                    
                 }
-               
             }
-            
         }
 
-        //player movement
-        if (move == true)
-        {
-            direction = waypoints[currentWP].transform.position - transform.position;
-            this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
-            this.transform.Translate(0, 0, Time.deltaTime * speed);
-        }
+       
 	}
 
 
