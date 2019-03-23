7c1f27abf401501fbe9629c261081d80f0ae081d
Cheenue Yang <31903854+YangCheenue@users.noreply.github.com>
2019-03-22 20:06:23 -0500

    Added Flare Vignette Scaling

---
+++ Wisper/Assets/FlareVignette.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class FlareVignette : MonoBehaviour {
+    
+    private Transform shrine;
+    public PostProcessingProfile postProcessingProfile;
+    private VignetteModel.Settings vignetteSettings;
+
+    public float defaultVignette = 0.2f;
+    private float finalVignette;
+
+    // Use this for initialization
+    void Start () {
+        shrine = GameObject.Find("Shrine").transform;
+        vignetteSettings = postProcessingProfile.vignette.settings;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float flareDot = Vector3.Dot((shrine.transform.position - transform.position).normalized, transform.forward.normalized);
+
+        finalVignette = defaultVignette + (1 - flareDot) * (.5f);
+
+        if (finalVignette > 0.5f)
+        {
+            finalVignette = 0.5f;
+        }
+        vignetteSettings.intensity = finalVignette;
+
+        Debug.Log("Flare Dot Product: " + flareDot + ", vignette intensity: " + finalVignette);
+
+
+        postProcessingProfile.vignette.settings = vignetteSettings;
+	}
+}
