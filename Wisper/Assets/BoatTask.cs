5643b1b34b1dc600789188722bcbf32cec7c03f3
Chris Schrempp <schremppc0870@my.uwstout.edu>
2019-04-04 00:38:03 -0500

    Messing around with NPC Movement

---
+++ Wisper/Assets/BoatTask.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatTask : MonoBehaviour
+{
+    public List<GameObject> waypoints;
+    int currentWP = 0;
+    public GameObject waypoint01;
+
+    //used components
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.AddForce(waypoint01.position - transform.position, 0.5);
+    }
+}
