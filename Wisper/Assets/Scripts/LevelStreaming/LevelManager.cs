b48390216463e5491a11f5d65f082117fbd04401
Chris Schrempp <schremppc0870@my.uwstout.edu>
2019-04-06 22:49:44 -0500

    LevelStreaming Proof of Concept

---
+++ Wisper/Assets/Scripts/LevelStreaming/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public static LevelManager Instance { set; get; }
+
+    void Awake()
+    {
+        Instance = this;
+        Load("RedOrb");
+    }
+
+    public void Load(string sceneName)
+    {
+        if (!SceneManager.GetSceneByName(sceneName).isLoaded)
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        }
+    }
+
+    public void Unload(string sceneName)
+    {
+        if (SceneManager.GetSceneByName(sceneName).isLoaded)
+        {
+            SceneManager.UnloadSceneAsync(sceneName);
+        }
+    }
+}
---
+++ Wisper/Assets/Scripts/LevelStreaming/LoadTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadTrigger : MonoBehaviour
+{
+
+    public string LoadName;
+    public string UnloadName;
+
+private void OnTriggerEnter(Collider col)
+    {
+        if (LoadName != "")
+        {
+            LevelManager.Instance.Load(LoadName);
+        }
+        if (UnloadName != "")
+        {
+            StartCoroutine("UnloadScene");
+        }
+    }
+
+    IEnumerator UnloadScene()
+    {
+        yield return new WaitForSeconds(0.10f);
+        LevelManager.Instance.Unload(UnloadName);
+    }
+}
