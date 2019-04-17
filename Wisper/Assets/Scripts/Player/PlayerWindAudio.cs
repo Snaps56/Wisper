e1381a8ee3e784d67d5fbbbe9203bc44952db43a
Snaps56 <nmnorton56@gmail.com>
2019-04-16 23:17:21 -0500

    Added a sound for the throwing

--- Wisper/Assets/Scripts/Player/PlayerWindAudio.cs
+++ Wisper/Assets/Scripts/Player/PlayerWindAudio.cs
@@ -9,13 +9,41 @@ public class PlayerWindAudio : MonoBehaviour {
     public AudioSource source;
     private float finalVelocity;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject abilitiesCollider;
+    public AudioClip throwAudioClip;
+
+    private AudioSource throwingAudioSource;
+    private bool isThrowing;
+
+    private bool soundPlayed = false;
+
+
+    // Use this for initialization
+    void Start () {
         source.volume = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    }
+
+    void Awake()
+    {
+        // add the necessary AudioSources:
+        throwingAudioSource = AddAudio(throwAudioClip, true, true, 1);
+
+    }
+
+    AudioSource AddAudio(AudioClip clip, bool loop, bool playAwake,  float vol) {
+        AudioSource newAudio = gameObject.AddComponent<AudioSource>();
+        newAudio.clip = clip;
+        newAudio.loop = loop;
+        newAudio.playOnAwake = playAwake;
+        newAudio.volume = vol;
+        return newAudio;
+    }
+
+// Update is called once per frame
+void Update () {
+        isThrowing = abilitiesCollider.GetComponent<ObjectThrow>().GetIsThrowingObjects();
+
         if (playerrigidbody.velocity.magnitude > 1)
         {
             if (1 - 1 / playerrigidbody.velocity.magnitude > 0.5)
@@ -29,5 +57,22 @@ public class PlayerWindAudio : MonoBehaviour {
             source.volume = 0.25f;
             source.pitch = 1f;
         }
+        throwingAudioSource.pitch = 1;
+        if (!isThrowing)
+        {
+            soundPlayed = true;
+        }
+        else if (isThrowing && !throwingAudioSource.isPlaying)
+        {
+            if (soundPlayed)
+            {
+                soundPlayed = false;
+                source.PlayOneShot(throwAudioClip);
+                soundPlayed = false;
+            }
+
+        }
+
+
 	}
 }
