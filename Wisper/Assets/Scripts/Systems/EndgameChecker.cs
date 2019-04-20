0da4601e55bcfdee58d35243bbcdf504593ca0db
Cheenue Yang <31903854+YangCheenue@users.noreply.github.com>
2019-04-20 12:39:50 -0500

    Added Endgame Checker Script

---
+++ Wisper/Assets/Scripts/Systems/EndgameChecker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndgameChecker : MonoBehaviour
+{
+    public string [] taskPSDVariables;
+    private bool allTasksDone = false;
+    private bool detectedTaskUnfinished = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Tilde))
+        {
+            allTasksDone = true;
+            Debug.Log("Debugged all PSD Tasks. All Tasks Done = " + allTasksDone);
+        }
+    }
+    public bool CheckAllTasksDone()
+    {
+        detectedTaskUnfinished = false;
+        for (int i = 0; i < taskPSDVariables.Length; i++)
+        {
+            if (!(bool)PersistantStateData.persistantStateData.stateConditions[taskPSDVariables[i]])
+            {
+                detectedTaskUnfinished = true;
+                break;
+            }
+        }
+        if (!detectedTaskUnfinished)
+        {
+            allTasksDone = true;
+        }
+        return allTasksDone;
+    }
+}
