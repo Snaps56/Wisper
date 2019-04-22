87397bc94cabc44c258ef23703fa63fd8e57ccca
JaceAD <36213892+JaceAD@users.noreply.github.com>
2019-04-22 14:36:42 -0500

    Gazebo orb spawn and PSD initialization

--- Wisper/Assets/Scripts/Task & Orb/Gazebo/Instrument.cs
+++ Wisper/Assets/Scripts/Task & Orb/Gazebo/Instrument.cs
@@ -6,5 +6,29 @@ public class Instrument : MonoBehaviour
 {
     public InstrumentType instrumentType;
     public Vector3 lastPos;
-   
+
+    private void Start()
+    {
+        if(instrumentType.Equals(InstrumentType.Drum))
+        {
+            if((bool)PersistantStateData.persistantStateData.stateConditions["DrumsGot"])
+            {
+                Destroy(this);
+            }
+        }
+        else if(instrumentType.Equals(InstrumentType.Saxophone))
+        {
+            if ((bool)PersistantStateData.persistantStateData.stateConditions["SaxGot"])
+            {
+                Destroy(this);
+            }
+        }
+        else if (instrumentType.Equals(InstrumentType.Tamborine))
+        {
+            if ((bool)PersistantStateData.persistantStateData.stateConditions["TamboGot"])
+            {
+                Destroy(this);
+            }
+        }
+    }
 }
