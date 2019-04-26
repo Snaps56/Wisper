50c29a0c33861f366dbb3bbd2d5b02a83b84137e
Snaps56 <nmnorton56@gmail.com>
2019-04-26 10:54:47 -0500

    Nick's Changes and Doug's Scene Additions

---
+++ Wisper/Assets/PowerBarPersistence.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerBarPersistence : MonoBehaviour
+{
+    public static GameObject powerbar;
+
+    private void Awake()
+    {
+        if (powerbar == null)
+        {
+            powerbar = this.gameObject;
+        }
+        else if (powerbar != this)
+        {
+            //Debug.Log("PSD static self reference not null. Destroying this.");
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
