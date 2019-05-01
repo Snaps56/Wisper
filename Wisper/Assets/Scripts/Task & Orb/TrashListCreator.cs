12a0553b763aa7a6da823691d46b6ff5378a139b
Daniel Larrea <larread6286@my.uwstout.edu>
2019-04-30 23:24:08 -0500

    Progress on Trash Task stuff
    -Made several prefabs for open dumpsters/garbage cans + groups of trash
    -Residential Area is complete for trash things
    -Updated TrashListCreator to have a more convenient list filling process, requires a TrashName now

--- Wisper/Assets/Scripts/Task & Orb/TrashListCreator.cs
+++ Wisper/Assets/Scripts/Task & Orb/TrashListCreator.cs
@@ -7,6 +7,7 @@ public class TrashListCreator : MonoBehaviour
 
     public List<GameObject> trashInArea = new List<GameObject>(); // List of trash that belongs to the area
     public string pSDVariable; // Name of the trash area's PersistentStateData variable, used to determine if corresponding trash should exist in the scene
+    public string trashName;
 
     // Use this for initialization
     void Start()
@@ -14,6 +15,18 @@ public class TrashListCreator : MonoBehaviour
         // Trash exists in scene at first
         // If the player has already cleaned the trash in the area
         // destroy the trash and clear the trashInArea list
+
+        List<GameObject> gameObjs = new List<GameObject>();
+        gameObjs.AddRange(GameObject.FindGameObjectsWithTag("PickUp"));
+
+        foreach (GameObject gameObj in gameObjs) {
+            if (gameObj.name.Contains(trashName)) {
+                trashInArea.Add(gameObj);
+            }
+        }
+
+        gameObjs.Clear();
+
         if ((bool)PersistantStateData.persistantStateData.stateConditions[pSDVariable])
         {
             Debug.Log("Destroy all objects");
