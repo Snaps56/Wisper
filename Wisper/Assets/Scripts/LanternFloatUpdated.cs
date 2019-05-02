1c5e5a70f0be93912a1d48ed2537746dc40a2a3a
Cheenue Yang <31903854+YangCheenue@users.noreply.github.com>
2019-05-02 16:00:56 -0500

    Fixed Broken Reference to Lantern Float Script

---
+++ Wisper/Assets/Scripts/LanternFloatUpdated.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class LanternFloatUpdated : MonoBehaviour
+{
+    //GameObjects
+    public GameObject lantern;
+    public GameObject player;
+    public GameObject ability;
+
+    //Looking at Variables
+    private bool isPulling;
+    public Camera mainCamera;
+    public float dotProductAngle = 0.9f;
+    public float deactivateDistance = 5;
+
+    // Vibration Variables
+    bool playerIndexSet = false;
+    private bool TaskisDone = false;
+    
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+    
+
+    //The camera's forward vector
+    private Vector3 cameraForward;
+
+    //The vector between the task and camera
+    private Vector3 toTask;
+
+    // PTSD
+    private PersistantStateData persistantStateData;
+
+    //Rigidbody
+    protected Rigidbody Rigidbody;
+
+    public Vector3 Uplift = new Vector3(0,1,0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        persistantStateData = GameObject.Find("PersistantStateData").GetComponent<PersistantStateData>();
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float distance = Vector3.Distance(player.transform.position, transform.position);
+
+        if (distance <= deactivateDistance)
+        {
+            if (TaskisDone == false && ability.GetComponent<ObjectLift>().GetIsLiftingObjects() == true)
+            {
+                Rigidbody.useGravity = false;
+                Rigidbody.AddForce(Uplift);
+            }
+        }
+    }
+}
