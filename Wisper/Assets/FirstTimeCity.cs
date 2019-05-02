506919c014f3b26dace7cc9d1b32889c182862c6
Cheenue Yang <31903854+YangCheenue@users.noreply.github.com>
2019-05-02 02:21:26 -0500

    Add PSD Checker for First Time City

---
+++ Wisper/Assets/FirstTimeCity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstTimeCity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!(bool)PersistantStateData.persistantStateData.stateConditions["PlayerEnteredCityFirstTime"])
+        {
+            PersistantStateData.persistantStateData.ChangeStateConditions("PlayerEnteredCityFirstTime", true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
