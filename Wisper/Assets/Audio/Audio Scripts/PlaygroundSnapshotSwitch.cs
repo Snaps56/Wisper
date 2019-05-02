d34545ebc73bbf28126ff705c2865cedbf63dfdc
JaceAD <36213892+JaceAD@users.noreply.github.com>
2019-05-02 14:25:43 -0500

    Playground snapshot switch

---
+++ Wisper/Assets/Audio/Audio Scripts/PlaygroundSnapshotSwitch.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class PlaygroundSnapshotSwitch : MonoBehaviour {
+    
+	public AudioMixerSnapshot onSnapshot; //Default
+    public AudioMixerSnapshot offSnapshot; //Snapshot on
+	public float fadeTime = 3.0f;
+    public float delayTime = 0.0f;
+    private AudioSource myAudioSource;
+    public AudioSource L2AudioSource;
+    private ActivateCutscene activateCutsceneScript;
+
+    bool lastCutsceneActiveState = false;
+
+
+	// Use this for initialization
+	void Start () {
+        activateCutsceneScript = GameObject.Find("CutsceneManager").GetComponent<ActivateCutscene>();
+        if(lastCutsceneActiveState != activateCutsceneScript.CutscenePlaying)
+        {
+            lastCutsceneActiveState = activateCutsceneScript.CutscenePlaying;
+            if(lastCutsceneActiveState == true)
+            {
+                // DO THING WITH OFF SNAPSHOT HERE TO TUNE DOWN THE VOLUME
+            }
+            else
+            {
+                // DO THING WITH ON SNAPSHOT HERE TO TUNE DOWN THE VOLUME
+            }
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (lastCutsceneActiveState != activateCutsceneScript.CutscenePlaying)
+        {
+            lastCutsceneActiveState = activateCutsceneScript.CutscenePlaying;
+            if (lastCutsceneActiveState == true)
+            {
+                // DO THING WITH OFF SNAPSHOT HERE TO TUNE DOWN THE VOLUME
+            }
+            else
+            {
+                // DO THING WITH ON SNAPSHOT HERE TO TUNE DOWN THE VOLUME
+            }
+        }
+
+    }
+    
+}
